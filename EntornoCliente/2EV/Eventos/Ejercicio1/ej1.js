141738135cfc070ab8093d7070fb08bb961343ef
rodriiii94 <rodrisantaga94@gmail.com>
2024-12-12 18:37:34 +0100

    EventosCliente

--- EntornoCliente/2EV/Eventos/Ejercicio1/ej1.js
+++ EntornoCliente/2EV/Eventos/Ejercicio1/ej1.js
@@ -2,8 +2,16 @@ document.addEventListener('mousemove', function(event) {
     muestraInformacion(event);
 });
 
+document.addEventListener('keydown', function(event) {
+    muestraInfoKey(event);
+});
+
+document.addEventListener('click', function(event) {
+    cambioColorRaton(event);
+});
+
 function muestraInformacion(event) {
-    const infoDiv = document.getElementById('info');
+    const infoDiv = document.getElementById('infoCur');
     const navegadorX = event.clientX;
     const navegadorY = event.clientY;
     const paginaX = event.screenX;
@@ -13,4 +21,26 @@ function muestraInformacion(event) {
         <p>Posición respecto del navegador: (${navegadorX}, ${navegadorY})</p>
         <p>Posición respecto de la página: (${paginaX}, ${paginaY})</p>
     `;
+}
+
+function cambioColorRaton(event) {
+    const infoDiv = document.getElementById('infoCur');
+    infoDiv.style.backgroundColor = '#FFFFCC';
+}
+
+function muestraInfoKey(event) {
+    const infoDiv = document.getElementById('infoKey');
+    const tecla = event.key;
+    const codigo = event.keyCode;
+    const char = event.charCode;
+    const evento = event.type;
+
+    infoDiv.innerHTML = `
+        <p>Tecla: ${tecla}</p>
+        <p>Código: ${codigo}</p>
+        <p>Char: ${char}</p>
+        <p>Evento: ${evento}</p>
+    `;
+
+    infoDiv.style.backgroundColor = '#CCE6FF';
 }
