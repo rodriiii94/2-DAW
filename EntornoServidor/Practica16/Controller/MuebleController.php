b66c70c5f7499bcbd99d0ef4498d2229e87eca67
rodriiii94 <rodrisantaga94@gmail.com>
2025-01-14 18:35:28 +0100

    NoFuncionaLoginLinux

--- EntornoServidor/Practica16/Controller/MuebleController.php
+++ EntornoServidor/Practica16/Controller/MuebleController.php
@@ -1,19 +1,5 @@
 <?php
 class MuebleController {
-    public function listarNombres() {
-        require_once '../Model/Mueble.php';
-        $mueble = new Mueble();
-        require_once '../View/listado.php';
-        return $mueble->get_nombre();
-    }
-
-    public function listarPrecios() {
-        require_once '../Model/Mueble.php';
-        $mueble = new Mueble();
-        require_once '../View/listado.php';
-        return $mueble->getPrecio();
-    }
-
     public function listarMuebles() {
         require_once '../Model/Mueble.php';
         $mueble = new Mueble();
